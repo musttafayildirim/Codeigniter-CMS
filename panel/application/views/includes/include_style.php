a0ae83c7b5c73a67574ac078ae54347757c3318a
Mustafa YILDIRIM <mustafa.yildirim1997@gmail.com>
2020-01-31 00:21:44 +0300

    dashboard teması projeye dahil edilip üzerinde boş bir content sayfası yapıldı.

---
+++ panel/application/views/includes/include_style.php
@@ -0,0 +1,15 @@
+<link rel="stylesheet" href="../panel/assets/libs/bower/font-awesome/css/font-awesome.min.css">
+<link rel="stylesheet" href="../panel/assets/libs/bower/material-design-iconic-font/dist/css/material-design-iconic-font.css">
+<!-- build:css ../assets/css/app.min.css -->
+<link rel="stylesheet" href="../panel/assets/libs/bower/animate.css/animate.min.css">
+<link rel="stylesheet" href="../panel/assets/libs/bower/fullcalendar/dist/fullcalendar.min.css">
+<link rel="stylesheet" href="../panel/assets/libs/bower/perfect-scrollbar/css/perfect-scrollbar.css">
+<link rel="stylesheet" href="../panel/assets/assets/css/bootstrap.css">
+<link rel="stylesheet" href="../panel/assets/assets/css/core.css">
+<link rel="stylesheet" href="../panel/assets/assets/css/app.css">
+<!-- endbuild -->
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway:400,500,600,700,800,900,300">
+<script src="../panel/assets/libs/bower/breakpoints.js/dist/breakpoints.min.js"></script>
+<script>
+    Breakpoints();
+</script>
