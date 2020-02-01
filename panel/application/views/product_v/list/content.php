97fe28ea80d2c321b96952c2f905f630ceef4e1b
Mustafa YILDIRIM <mustafa.yildirim1997@gmail.com>
2020-02-02 02:10:19 +0300

    ürünler listesi için html şablonu oluşturuldu.

--- panel/application/views/product_v/list/content.php
+++ panel/application/views/product_v/list/content.php
@@ -1,1 +1,84 @@
-product list
+<div class="row">
+
+    <div class="col-md-12">
+        <div class="widget">
+            <header class="widget-header">
+                <h4 class="widget-title">Ürün Listesi</h4>
+            </header><!-- .widget-header -->
+            <hr class="widget-separator">
+            <div class="widget-body">
+                <div class="table-responsive">
+                    <table id="default-datatable" data-plugin="DataTable" class="table table-striped" cellspacing="0" width="100%">
+                        <thead>
+                        <tr>
+                            <th>#id</th>
+                            <th>URL</th>
+                            <th>Başlık</th>
+                            <th>Açıklama</th>
+                            <th>Durum</th>
+                            <th>İşlem</th>
+                        </tr>
+                        </thead>
+                        <tfoot>
+                        <tr>
+                            <th>#id</th>
+                            <th>URL</th>
+                            <th>Başlık</th>
+                            <th>Açıklama</th>
+                            <th>Durum</th>
+                            <th>İşlem</th>
+                        </tr>
+                        </tfoot>
+                        <tbody>
+                        <tr>
+                            <td>Donna Snider</td>
+                            <td>Customer Support</td>
+                            <td>New York</td>
+                            <td>27</td>
+                            <td>1/0</td>
+                            <td>
+                                <a href="#" class="btn btn-danger mw-xs"><i class="fa fa-trash-o"></i></a>
+                                <a href="#" class="btn btn-info mw-xs"><i class="fa fa-pencil-square-o"></i></a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Donna Snider</td>
+                            <td>Customer Support</td>
+                            <td>New York</td>
+                            <td>27</td>
+                            <td>1/0</td>
+                            <td>
+                                <a href="#" class="btn btn-danger mw-xs"><i class="fa fa-trash-o"></i></a>
+                                <a href="#" class="btn btn-info mw-xs"><i class="fa fa-pencil-square-o"></i></a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Donna Snider</td>
+                            <td>Customer Support</td>
+                            <td>New York</td>
+                            <td>27</td>
+                            <td>1/0</td>
+                            <td>
+                                <a href="#" class="btn btn-danger mw-xs"><i class="fa fa-trash-o"></i></a>
+                                <a href="#" class="btn btn-info mw-xs"><i class="fa fa-pencil-square-o"></i></a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Donna Snider</td>
+                            <td>Customer Support</td>
+                            <td>New York</td>
+                            <td>27</td>
+                            <td>1/0</td>
+                            <td>
+                                <a href="#" class="btn btn-danger mw-xs"><i class="fa fa-trash-o"></i></a>
+                                <a href="#" class="btn btn-info mw-xs"><i class="fa fa-pencil-square-o"></i></a>
+                            </td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div><!-- .widget-body -->
+        </div><!-- .widget -->
+    </div><!-- END column -->
+
+</div>
