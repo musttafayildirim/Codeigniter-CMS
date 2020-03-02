329554523da907842c12c3ef5960156aa03de70f
Mustafa YILDIRIM <mustafa.yildirim1997@gmail.com>
2020-03-02 23:59:06 +0300

    Kullanıcının login olma işlemi gerçekleştirildi ve buna göre hata durumlarında böyle bir kullanıcı bulunamadı, şifreniz yanlış tarzı mesajlar verildi.

--- panel/application/controllers/UserOperation.php
+++ panel/application/controllers/UserOperation.php
@@ -9,17 +9,7 @@ class UserOperation extends CI_Controller
     {
         parent::__construct();
         $this->viewFolder = "users_v";
-    }
-
-    public function index()
-    {
-        $viewData = new stdClass();
-
-        //View'e gönderilecek değişkenlerin set edilmesi...
-        $viewData-> viewFolder = $this->viewFolder;
-        $viewData->subViewFolder = "login";
-
-        $this->load->view("{$viewData->viewFolder}/{$viewData->subViewFolder}/index", $viewData);
+        $this->load->model("user_model");
     }
 
     public function login()
@@ -31,6 +21,84 @@ class UserOperation extends CI_Controller
 
         $this->load->view("{$viewData->viewFolder}/{$viewData->subViewFolder}/index", $viewData);
     }
+
+    public function do_login(){
+        $this->load->library("form_validation");
+
+        $this->form_validation->set_rules("user_name", "Kullanıcı Adı", "required|trim");
+        $this->form_validation->set_rules("password", "Şifre", "required|trim");
+
+        $this->form_validation->set_message(
+            array(
+                "required" => "<strong>{field}</strong> alanı doldurulmalıdır.",
+            )
+        );
+
+        if($this->form_validation->run() == FALSE){
+            $viewData = new stdClass();
+
+            $viewData-> viewFolder = $this->viewFolder;
+            $viewData->subViewFolder = "login";
+            $viewData->form_error = "true";
+
+            $alert = array(
+                "title"   => "İşlem başarısız",
+                "text"    => "Lütfen bilgilerinizi kontrol ediniz!",
+                "type"    => "error"
+            );
+
+            $this->session->set_flashdata("alert", $alert);
+            $this->load->view("{$viewData->viewFolder}/{$viewData->subViewFolder}/index", $viewData);
+            unset($_SESSION['alert']);
+
+        }
+        else{
+            $username = $this->user_model->get(
+                array(
+                    "user_name" =>  $this->input->post("user_name"),
+                )
+            );
+            $user =  $this->user_model->get(
+                array(
+                    "user_name" =>  $this->input->post("user_name"),
+                    "password"  => md5($this->input->post("password"))
+                )
+            );
+
+                if($username) {
+                        if ($user) {
+                            $alert = array(
+                                "title" => "İşlem başarılı",
+                                "text" => "$user->full_name hoşgeldiniz",
+                                "type" => "success"
+                            );
+                            $this->session->set_userdata("user", $user);
+                            $this->session->set_flashdata("alert", $alert);
+                            redirect(base_url());
+                        } else {
+                            $alert = array(
+                                "title" => "İşlem başarısız",
+                                "text" => "Lütfen şifrenizi kontrol ediniz!",
+                                "type" => "error"
+                            );
+                            $this->session->set_flashdata("alert", $alert);
+                            redirect(base_url("login"));
+                            unset($_SESSION['alert']);
+                        }
+                }
+                else{
+                    $alert = array(
+                        "title"   => "İşlem başarısız",
+                        "text"    => "Böyle bir kullanıcı bulunamadı!",
+                        "type"    => "error"
+                    );
+                    $this->session->set_flashdata("alert", $alert);
+                    redirect(base_url("login"));
+                    unset($_SESSION['alert']);
+                }
+        }
+    }
+
 
 
 }
--- panel/application/views/users_v/login/content.php
+++ panel/application/views/users_v/login/content.php
@@ -6,13 +6,19 @@
 </div><!-- logo -->
 <div class="simple-page-form animated flipInY" id="login-form">
     <h4 class="form-title m-b-xl text-center">Bilgilerinizle Giriş Yapınız</h4>
-    <form action="#">
+    <form action="<?php echo base_url('useroperation/do_login'); ?>" method="post">
         <div class="form-group">
-            <input id="sign-in-email" type="email" class="form-control" placeholder="Email adresinizi giriniz">
+            <input id="sign-in-email" type="text" class="form-control" name="user_name" placeholder="Kullanıcı Adınızı Giriniz">
+            <?php if(isset($form_error)){ ?>
+                <small class="pull-right input-form-error"><?php echo form_error( "user_name");?></small>
+            <?php } ?>
         </div>
 
         <div class="form-group">
-            <input id="sign-in-password" type="password" class="form-control" placeholder="Şifrenizi Giriniz">
+            <input id="sign-in-password" type="password" class="form-control" name="password" placeholder="Şifrenizi Giriniz">
+            <?php if(isset($form_error)){ ?>
+                <small class="pull-right input-form-error"><?php echo form_error( "password");?></small>
+            <?php } ?>
         </div>
 
         <input type="submit" class="btn btn-primary" value="Giriş Yap">
