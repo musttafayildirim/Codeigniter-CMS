a65662b4fd3fa0e128d38ecaadfb44a95fa1914a
Mustafa YILDIRIM <mustafa.yildirim1997@gmail.com>
2020-03-18 11:06:00 +0300

    Site ayarları yapıldı boş sayfa oluşturuldu ve artık frond-end e geçildi.

---
+++ site/application/views/includes/footer.php
@@ -0,0 +1,188 @@
+<div class="default-bg footer-top border-clear">
+
+</div>
+<footer id="footer" class="clearfix dark">
+
+    <!-- .footer start -->
+    <!-- ================ -->
+    <div class="footer">
+        <div class="container">
+            <div class="footer-inner">
+                <div class="row">
+                    <div class="col-lg-3">
+                        <div class="footer-content">
+                            <div class="logo-footer"><img id="logo-footer" src="<?php echo base_url("assets")?>/images/logo_purple.png" alt=""></div>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus illo vel dolorum soluta consectetur doloribus sit. Delectus non tenetur odit dicta vitae debitis suscipit doloribus. Ipsa, aut voluptas quaerat... <a href="page-about.html">Learn More<i class="fa fa-long-arrow-right pl-1"></i></a></p>
+                            <div class="separator-2"></div>
+                            <nav>
+                                <ul class="nav flex-column">
+                                    <li class="nav-item"><a class="nav-link" target="_blank" href="http://htmlcoder.me/support">Support</a></li>
+                                    <li class="nav-item"><a class="nav-link" href="#">Privacy</a></li>
+                                    <li class="nav-item"><a class="nav-link" href="#">Terms</a></li>
+                                    <li class="nav-item"><a class="nav-link" href="page-about.html">About</a></li>
+                                </ul>
+                            </nav>
+                        </div>
+                    </div>
+                    <div class="col-lg-3">
+                        <div class="footer-content">
+                            <h2 class="title">Latest From Blog</h2>
+                            <div class="separator-2"></div>
+                            <div class="media margin-clear">
+                                <div class="d-flex pr-2">
+                                    <div class="overlay-container">
+                                        <img class="media-object" src="<?php echo base_url("assets")?>/images/blog-thumb-1.jpg" alt="blog-thumb">
+                                        <a href="blog-post.html" class="overlay-link small"><i class="fa fa-link"></i></a>
+                                    </div>
+                                </div>
+                                <div class="media-body">
+                                    <h6 class="media-heading"><a href="blog-post.html">Lorem ipsum dolor sit amet...</a></h6>
+                                    <p class="small margin-clear"><i class="fa fa-calendar pr-2"></i>Mar 23, 2017</p>
+                                </div>
+                            </div>
+                            <hr>
+                            <div class="media margin-clear">
+                                <div class="d-flex pr-2">
+                                    <div class="overlay-container">
+                                        <img class="media-object" src="<?php echo base_url("assets")?>/images/blog-thumb-2.jpg" alt="blog-thumb">
+                                        <a href="blog-post.html" class="overlay-link small"><i class="fa fa-link"></i></a>
+                                    </div>
+                                </div>
+                                <div class="media-body">
+                                    <h6 class="media-heading"><a href="blog-post.html">Lorem ipsum dolor sit amet...</a></h6>
+                                    <p class="small margin-clear"><i class="fa fa-calendar pr-2"></i>Mar 22, 2017</p>
+                                </div>
+                            </div>
+                            <hr>
+                            <div class="media margin-clear">
+                                <div class="d-flex pr-2">
+                                    <div class="overlay-container">
+                                        <img class="media-object" src="<?php echo base_url("assets")?>/images/blog-thumb-3.jpg" alt="blog-thumb">
+                                        <a href="blog-post.html" class="overlay-link small"><i class="fa fa-link"></i></a>
+                                    </div>
+                                </div>
+                                <div class="media-body">
+                                    <h6 class="media-heading"><a href="blog-post.html">Lorem ipsum dolor sit amet...</a></h6>
+                                    <p class="small margin-clear"><i class="fa fa-calendar pr-2"></i>Mar 21, 2017</p>
+                                </div>
+                            </div>
+                            <hr>
+                            <div class="media margin-clear">
+                                <div class="d-flex pr-2">
+                                    <div class="overlay-container">
+                                        <img class="media-object" src="<?php echo base_url("assets")?>/images/blog-thumb-4.jpg" alt="blog-thumb">
+                                        <a href="blog-post.html" class="overlay-link small"><i class="fa fa-link"></i></a>
+                                    </div>
+                                </div>
+                                <div class="media-body">
+                                    <h6 class="media-heading"><a href="blog-post.html">Lorem ipsum dolor sit amet...</a></h6>
+                                    <p class="small margin-clear"><i class="fa fa-calendar pr-2"></i>Mar 21, 2017</p>
+                                </div>
+                            </div>
+                            <div class="text-right space-top">
+                                <a href="blog-large-image-right-sidebar.html" class="link-dark"><i class="fa fa-plus-circle pl-1 pr-1"></i>More</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3">
+                        <div class="footer-content">
+                            <h2 class="title">Portfolio Gallery</h2>
+                            <div class="separator-2"></div>
+                            <div class="row grid-space-10">
+                                <div class="col-4 col-lg-6">
+                                    <div class="overlay-container mb-10">
+                                        <img src="<?php echo base_url("assets")?>/images/gallery-1.jpg" alt="">
+                                        <a href="portfolio-item.html" class="overlay-link small">
+                                            <i class="fa fa-link"></i>
+                                        </a>
+                                    </div>
+                                </div>
+                                <div class="col-4 col-lg-6">
+                                    <div class="overlay-container mb-10">
+                                        <img src="<?php echo base_url("assets")?>/images/gallery-2.jpg" alt="">
+                                        <a href="portfolio-item.html" class="overlay-link small">
+                                            <i class="fa fa-link"></i>
+                                        </a>
+                                    </div>
+                                </div>
+                                <div class="col-4 col-lg-6">
+                                    <div class="overlay-container mb-10">
+                                        <img src="<?php echo base_url("assets")?>/images/gallery-3.jpg" alt="">
+                                        <a href="portfolio-item.html" class="overlay-link small">
+                                            <i class="fa fa-link"></i>
+                                        </a>
+                                    </div>
+                                </div>
+                                <div class="col-4 col-lg-6">
+                                    <div class="overlay-container mb-10">
+                                        <img src="<?php echo base_url("assets")?>/images/gallery-4.jpg" alt="">
+                                        <a href="portfolio-item.html" class="overlay-link small">
+                                            <i class="fa fa-link"></i>
+                                        </a>
+                                    </div>
+                                </div>
+                                <div class="col-4 col-lg-6">
+                                    <div class="overlay-container mb-10">
+                                        <img src="<?php echo base_url("assets")?>/images/gallery-5.jpg" alt="">
+                                        <a href="portfolio-item.html" class="overlay-link small">
+                                            <i class="fa fa-link"></i>
+                                        </a>
+                                    </div>
+                                </div>
+                                <div class="col-4 col-lg-6">
+                                    <div class="overlay-container mb-10">
+                                        <img src="<?php echo base_url("assets")?>/images/gallery-6.jpg" alt="">
+                                        <a href="portfolio-item.html" class="overlay-link small">
+                                            <i class="fa fa-link"></i>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="text-right space-top">
+                                <a href="portfolio-grid-2-3-col.html" class="link-dark"><i class="fa fa-plus-circle pl-1 pr-1"></i>More</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3">
+                        <div class="footer-content">
+                            <h2 class="title">Find Us</h2>
+                            <div class="separator-2"></div>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium odio voluptatem necessitatibus illo vel dolorum soluta.</p>
+                            <ul class="social-links circle animated-effect-1">
+                                <li class="facebook"><a target="_blank" href="http://www.facebook.com"><i class="fa fa-facebook"></i></a></li>
+                                <li class="twitter"><a target="_blank" href="http://www.twitter.com"><i class="fa fa-twitter"></i></a></li>
+                                <li class="googleplus"><a target="_blank" href="http://plus.google.com"><i class="fa fa-google-plus"></i></a></li>
+                                <li class="linkedin"><a target="_blank" href="http://www.linkedin.com"><i class="fa fa-linkedin"></i></a></li>
+                                <li class="xing"><a target="_blank" href="http://www.xing.com"><i class="fa fa-xing"></i></a></li>
+                            </ul>
+                            <div class="separator-2"></div>
+                            <ul class="list-icons">
+                                <li><i class="fa fa-map-marker pr-2 text-default"></i> One infinity loop, 54100</li>
+                                <li><i class="fa fa-phone pr-2 text-default"></i> +00 1234567890</li>
+                                <li><a href="mailto:info@theproject.com"><i class="fa fa-envelope-o pr-2"></i>info@theproject.com</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- .footer end -->
+
+    <!-- .subfooter start -->
+    <!-- ================ -->
+    <div class="subfooter">
+        <div class="container">
+            <div class="subfooter-inner">
+                <div class="row">
+                    <div class="col-md-12">
+                        <p class="text-center">Copyright © 2020 Mustafa YILDIRIM <a target="_blank" href="<?php echo base_url(); ?>">buraya tıkla</a>. Tüm Hakları Saklıdır.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- .subfooter end -->
+
+</footer>
+<!-- footer end -->
---
+++ site/application/views/includes/head.php
@@ -0,0 +1,9 @@
+<meta charset="utf-8">
+<title>The Project | Home</title>
+<meta name="description" content="The Project a Bootstrap-based, Responsive HTML5 Template">
+<meta name="author" content="htmlcoder.me">
+
+<!-- Mobile Meta -->
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<?php $this->load->view("includes/include_style") ?>
